9960e5b1526da6d7cc0cbbb058fed6dec4909db1
Devender Gollapally <devender@startengine.com>
2025-11-04 18:06:30 -0800

    Bump version to 1.0.2

--- startengine_erc1450/__init__.py
+++ startengine_erc1450/__init__.py
@@ -5,7 +5,7 @@ Provides access to compiled smart contract artifacts and deployment utilities
 for the ERC1450 token standard and RTAProxy multi-signature contracts.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "StartEngine"
 
 from .artifacts.loader import (
